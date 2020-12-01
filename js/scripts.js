0ba35a6ac2ca54ae6a7ed755740d89ff40aae506
angga <anggaaap20@gmail.com>
2020-12-01 09:48:07 +0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "https://i.gyazo.com/1bc057f1675b02ec1af2b9cacd54f446.jpg"
+    , "https://i.gyazo.com/468e63af9b5e1935185fd285836c2b8a.jpg"
+    , "https://i.gyazo.com/c692373c0245a867677109a9a7b307aa.jpg"
+    , "https://i.gyazo.com/5a3848e02638c255057ffb6a49ccf2b2.jpg"
+    ], {duration: 3000, fade: 750});
+});
