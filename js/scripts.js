ca23ad96b5880e1b1d10575d357788d28fc0373a
dariuuz17 <yuliusmankurniawan@gmail.com>
2021-06-18 11:07:48 +0700

    Commit Pertama

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ jQuery(document).ready(function() {
         Background slideshow
     */
     $('#banner').backstretch([
-      "https://i.gyazo.com/1bc057f1675b02ec1af2b9cacd54f446.jpg"
-    , "https://i.gyazo.com/468e63af9b5e1935185fd285836c2b8a.jpg"
-    , "https://i.gyazo.com/c692373c0245a867677109a9a7b307aa.jpg"
-    , "https://i.gyazo.com/5a3848e02638c255057ffb6a49ccf2b2.jpg"
+      "https://i.gyazo.com/fa6c0798dedb9e5d9e149e1a5b22addf.jpg"
+    , "https://i.gyazo.com/68393486d0ccb2febce73453f3202caa.jpg"
+    , "https://i.gyazo.com/809cb5dfdc1968270dd324b1d2a71ac3.jpg"
+    , "https://i.gyazo.com/f2fbad39e3b8c02a941d85079c10d129.jpg"
     ], {duration: 3000, fade: 750});
 });
