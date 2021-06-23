e6dc9454261760690ae1468c15901538f0cbc63b
dariuuz17 <yuliusmankurniawan@gmail.com>
2021-06-23 11:46:18 +0700

    commit pertama

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,6 @@ jQuery(document).ready(function () {
     "https://i.gyazo.com/fa6c0798dedb9e5d9e149e1a5b22addf.jpg"
     , "https://i.gyazo.com/68393486d0ccb2febce73453f3202caa.jpg"
     , "https://i.gyazo.com/06b8243b7fff661d55b9963666949148.jpg"
-    , "https://i.gyazo.com/f9b57ef285342010d05c19925f45af0e.jpg"
+    , "https://i.gyazo.com/7c33458625e9cce56f5cf339d3572794.jpg"
   ], { duration: 3000, fade: 750 });
 });
