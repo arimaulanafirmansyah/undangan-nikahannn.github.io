3ff23f0e1e36e2ed903bdb9cc4ad13daba8f5e52
dariuuz17 <yuliusmankurniawan@gmail.com>
2021-06-23 17:25:38 +0700

    commit ke 5

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function () {
   $('#banner').backstretch([
     "https://i.gyazo.com/fa6c0798dedb9e5d9e149e1a5b22addf.jpg"
     , "https://i.gyazo.com/68393486d0ccb2febce73453f3202caa.jpg"
-    , "https://i.gyazo.com/06b8243b7fff661d55b9963666949148.jpg"
+    , "https://i.gyazo.com/801c4545dd046a77fc11a09204f7b95c.jpg"
     , "https://i.gyazo.com/7c33458625e9cce56f5cf339d3572794.jpg"
   ], { duration: 3000, fade: 750 });
 });
