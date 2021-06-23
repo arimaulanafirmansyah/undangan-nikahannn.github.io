f6847c3b6f2cb123903b156344e2951f59ba09fe
dariuuz17 <yuliusmankurniawan@gmail.com>
2021-06-23 10:55:53 +0700

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-    /*
-        Background slideshow
-    */
-    $('#banner').backstretch([
-      "https://i.gyazo.com/fa6c0798dedb9e5d9e149e1a5b22addf.jpg"
+  /*
+      Background slideshow
+  */
+  $('#banner').backstretch([
+    "https://i.gyazo.com/fa6c0798dedb9e5d9e149e1a5b22addf.jpg"
     , "https://i.gyazo.com/68393486d0ccb2febce73453f3202caa.jpg"
-    , "https://i.gyazo.com/809cb5dfdc1968270dd324b1d2a71ac3.jpg"
-    , "https://i.gyazo.com/f2fbad39e3b8c02a941d85079c10d129.jpg"
-    ], {duration: 3000, fade: 750});
+    , "https://i.gyazo.com/06b8243b7fff661d55b9963666949148.jpg"
+    , "https://i.gyazo.com/f9b57ef285342010d05c19925f45af0e.jpg"
+  ], { duration: 3000, fade: 750 });
 });
